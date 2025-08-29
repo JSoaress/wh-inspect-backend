2e191aae7bd6d2bfe20f69174af2439af535eb07
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-29 00:06:14 -0300

    feat: encapsula lib zod

---
+++ src/infra/libs/zod/index.ts
@@ -0,0 +1,32 @@
+import * as z from "zod/v4";
+
+z.config(z.locales.pt());
+
+type SuccessResult<T> = {
+    success: true;
+    data: T;
+};
+
+type ErrorResult = {
+    success: false;
+    errors: Record<string, string[]>;
+};
+
+type ResultValidate<T> = SuccessResult<T> | ErrorResult;
+
+export class ZodValidator {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    static validate<I, O>(props: unknown, schema: z.ZodType<O, I>): ResultValidate<O> {
+        const result = schema.safeParse(props);
+        if (result.success) return { success: true, data: result.data };
+        const errors: Record<string, string[]> = {};
+        result.error.issues.forEach((issue) => {
+            const path = issue.path.join(".");
+            if (errors[path]) errors[path].push(issue.message);
+            else errors[path] = [issue.message];
+        });
+        return { success: false, errors };
+    }
+}
+
+export { z };
