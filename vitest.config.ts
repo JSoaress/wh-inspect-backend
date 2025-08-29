e6175df5972b4014e9f696047f25628a8b3fb4aa
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-29 12:37:26 -0300

    chore: ajusta config vitest com env e testTimeout: 10000

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,17 @@
-// import dotenv from "dotenv";
+/* eslint-disable import/no-extraneous-dependencies */
+import dotenv from "dotenv";
 import path from "node:path";
 import { defineConfig } from "vitest/config";
 
-// dotenv.config({ path: "./.env.test.local" });
+dotenv.config({ path: "./.env.test.local" });
 
 export default defineConfig({
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
+    },
+    test: {
+        testTimeout: 10000,
     },
 });
