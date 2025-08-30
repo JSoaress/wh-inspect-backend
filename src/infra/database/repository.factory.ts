79fed7849d2091fe2196311bba5cdbba6e4a6175
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-29 21:35:10 -0300

    chore(infra/db): ajusta classe de erro RepositoryFactory

--- src/infra/database/repository.factory.ts
+++ src/infra/database/repository.factory.ts
@@ -2,6 +2,7 @@ import { UnitOfWork } from "ts-arch-kit/dist/database";
 
 import { IProjectRepository, IWebhookLogRepository } from "@/app/projects/application/repos";
 import { IUserRepository } from "@/app/users/application/repos";
+import { MissingDependencyError } from "@/shared/errors";
 
 import { PgRepositoryFactory } from "./postgres/repository.factory";
 
@@ -18,7 +19,7 @@ export class RepositoryFactory {
             case "postgres":
                 return new PgRepositoryFactory();
             default:
-                throw new Error("Implementar");
+                throw new MissingDependencyError("IRepositoryFactory");
         }
     }
 }
