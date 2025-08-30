c9e5e082b03bcf4b2f88657e9405f7a127fdfd86
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-30 00:05:20 -0300

    feat(infra/http): add middleware 'authorizationBasedUser'

--- src/infra/http/express/middlewares/authorization.middleware.ts
+++ src/infra/http/express/middlewares/authorization.middleware.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 import { InvalidTokenError } from "@/app/_common";
 import { CheckAuthenticatedUserUseCase } from "@/app/users/application/use-cases/users/check-authenticated-user";
+import { GetUserByIdUseCase } from "@/app/users/application/use-cases/users/get-user-by-id";
 
 export function authorization(useCase: CheckAuthenticatedUserUseCase) {
     return async (req: Request, res: Response, next: NextFunction) => {
@@ -9,6 +10,16 @@ export function authorization(useCase: CheckAuthenticatedUserUseCase) {
         if (!header) return next(new InvalidTokenError("Token de autenticação não informado."));
         const [, authToken] = header.split(" ");
         const result = await useCase.execute({ token: authToken });
+        if (result.isLeft()) return next(result.value);
+        req.requestUser = result.value;
+        return next();
+    };
+}
+
+export function authorizationBasedUser(useCase: GetUserByIdUseCase) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const { user } = req.params;
+        const result = await useCase.execute({ id: user });
         if (result.isLeft()) return next(result.value);
         req.requestUser = result.value;
         return next();
