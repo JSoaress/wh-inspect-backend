0d03ef390767e65a75e2b3c72eefc9cf2fbfaf72
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-29 21:36:25 -0300

    feat(shared): ajusta tratativas de env vars

--- src/shared/config/environment.ts
+++ src/shared/config/environment.ts
@@ -3,10 +3,10 @@ import envVar from "env-var";
 export const env = {
     NODE_ENV: envVar.get("NODE_ENV").default("development").asString(),
     PLATFORM_NAME: envVar.get("PLATFORM_NAME").default("WebhookInspec").asString(),
-    WEB_URL: envVar.get("WEB_URL").asUrlString(),
-    MAIL_PROVIDER: envVar.get("MAIL_PROVIDER").asString(),
-    MAIL_HOST: envVar.get("MAIL_HOST").asString(),
-    MAIL_PORT: envVar.get("MAIL_PORT").asInt(),
-    MAIL_USER: envVar.get("MAIL_USER").asString(),
-    MAIL_PASS: envVar.get("MAIL_PASS").asString(),
+    WEB_URL: envVar.get("WEB_URL").required().asUrlString(),
+    MAIL_PROVIDER: envVar.get("MAIL_PROVIDER").required().asString(),
+    MAIL_HOST: envVar.get("MAIL_HOST").required().asString(),
+    MAIL_PORT: envVar.get("MAIL_PORT").required().asInt(),
+    MAIL_USER: envVar.get("MAIL_USER").required().asString(),
+    MAIL_PASS: envVar.get("MAIL_PASS").required().asString(),
 };
