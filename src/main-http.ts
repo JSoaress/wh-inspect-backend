b5387a70ef7b3a70a25f3264333a4ba2b73337c5
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-29 23:00:01 -0300

    feat: ajusta dependencias no main http

--- src/main-http.ts
+++ src/main-http.ts
@@ -1,4 +1,5 @@
 import { UseCaseFactory } from "./app/_common/application";
+import { JsonWebToken } from "./infra/adapters/jwt";
 import { RepositoryFactory } from "./infra/database";
 import { ExpressHttpServer, HttpController } from "./infra/http";
 import { MailFactory } from "./infra/providers/mail";
@@ -7,7 +8,8 @@ import { env } from "./shared/config/environment";
 async function bootstrap() {
     const repositoryFactory = RepositoryFactory.getRepository("postgres");
     const mail = MailFactory.getMail(env.MAIL_PROVIDER);
-    const useCaseFactory = new UseCaseFactory(repositoryFactory, mail);
+    const jwt = new JsonWebToken();
+    const useCaseFactory = new UseCaseFactory(repositoryFactory, mail, jwt);
     const httpServer = new ExpressHttpServer("/api", useCaseFactory);
     const httpController = new HttpController(httpServer);
     httpController.setup();
