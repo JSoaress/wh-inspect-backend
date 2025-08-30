bc1115caf732865d94a4336e3d41ca2dfd68c12c
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-29 22:56:42 -0300

    feat(app/common): registra novos use case no UseCaseFactory

--- src/app/_common/application/usecase-factory.ts
+++ src/app/_common/application/usecase-factory.ts
@@ -1,10 +1,14 @@
+import { CreateProjectUseCase } from "@/app/projects/application/use-cases/projects/create-project";
 import { ActivateUserUseCase } from "@/app/users/application/use-cases/users/activate-user";
+import { AuthenticateUserUseCase } from "@/app/users/application/use-cases/users/authenticate-user";
+import { CheckAuthenticatedUserUseCase } from "@/app/users/application/use-cases/users/check-authenticated-user";
 import { CreateUserUseCase } from "@/app/users/application/use-cases/users/create-user";
+import { JsonWebToken } from "@/infra/adapters/jwt";
 import { IRepositoryFactory } from "@/infra/database";
 import { IMail } from "@/infra/providers/mail";
 
 export class UseCaseFactory {
-    constructor(private repositoryFactory: IRepositoryFactory, private mail: IMail) {}
+    constructor(private repositoryFactory: IRepositoryFactory, private mail: IMail, private jwt: JsonWebToken) {}
 
     createUserUseCase() {
         return new CreateUserUseCase({ repositoryFactory: this.repositoryFactory, mail: this.mail });
@@ -12,5 +16,17 @@ export class UseCaseFactory {
 
     activateUserUseCase() {
         return new ActivateUserUseCase({ repositoryFactory: this.repositoryFactory });
+    }
+
+    authenticateUserUseCase() {
+        return new AuthenticateUserUseCase({ repositoryFactory: this.repositoryFactory, jwt: this.jwt });
+    }
+
+    checkAuthenticatedUserUseCase() {
+        return new CheckAuthenticatedUserUseCase({ repositoryFactory: this.repositoryFactory, jwt: this.jwt });
+    }
+
+    createProjectUseCase() {
+        return new CreateProjectUseCase({ repositoryFactory: this.repositoryFactory });
     }
 }
