3727a664a6f789d42f6e0df3a8911abedbcf38ff
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-29 21:31:21 -0300

    feat(infra/presenters): add json presenters

---
+++ src/infra/presenters/json/index.ts
@@ -0,0 +1,20 @@
+import { IPresenter } from "ts-arch-kit/dist/core/helpers";
+
+import { User, UserDTO } from "@/app/users/domain/models/user";
+
+type UserJson = Omit<UserDTO, "password" | "userToken">;
+
+class UserJsonPresenter implements IPresenter<User, UserJson> {
+    present(input: User): UserJson {
+        return {
+            id: input.getId(),
+            name: input.name,
+            email: input.email,
+            cliToken: input.cliToken,
+            createdAt: input.createdAt,
+            isActive: input.isActive,
+        };
+    }
+}
+
+export const userJsonPresenter = new UserJsonPresenter();
