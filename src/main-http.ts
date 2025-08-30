e15948af9b78847d7e6a74c2f2d375945ade88dc
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-30 12:18:05 -0300

    feat: ajusta dependencias no main http

--- src/main-http.ts
+++ src/main-http.ts
@@ -1,5 +1,8 @@
+import http from "http";
+
 import { UseCaseFactory } from "./app/_common/application";
 import { JsonWebToken } from "./infra/adapters/jwt";
+import { SimpleWebSocket } from "./infra/adapters/ws";
 import { RepositoryFactory } from "./infra/database";
 import { ExpressHttpServer, HttpController } from "./infra/http";
 import { MailFactory } from "./infra/providers/mail";
@@ -9,11 +12,17 @@ async function bootstrap() {
     const repositoryFactory = RepositoryFactory.getRepository("postgres");
     const mail = MailFactory.getMail(env.MAIL_PROVIDER);
     const jwt = new JsonWebToken();
-    const useCaseFactory = new UseCaseFactory(repositoryFactory, mail, jwt);
+    const ws = new SimpleWebSocket();
+    const useCaseFactory = new UseCaseFactory(repositoryFactory, mail, jwt, ws);
+    ws.setGetUserUseCase(useCaseFactory.getUserUseCase());
     const httpServer = new ExpressHttpServer("/api", useCaseFactory);
+    const app = httpServer.getServer();
+    const server = http.createServer(app);
+    ws.start(server);
     const httpController = new HttpController(httpServer);
     httpController.setup();
-    httpServer.listen(3333);
+    const port = 3333;
+    server.listen(port, () => console.info(`Server ready and running on port ${port} with express.`));
 }
 
 bootstrap();
