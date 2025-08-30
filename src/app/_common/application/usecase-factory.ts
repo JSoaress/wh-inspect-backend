203fed2d1d85fcf94465c5fe334d3b55731d4760
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-30 00:05:58 -0300

    feat(app/common): registra novos use cases em UseCaseFactory

--- src/app/_common/application/usecase-factory.ts
+++ src/app/_common/application/usecase-factory.ts
@@ -1,8 +1,11 @@
 import { CreateProjectUseCase } from "@/app/projects/application/use-cases/projects/create-project";
+import { ForwardWebhookUseCase } from "@/app/projects/application/use-cases/webhooks/forward-webhook";
+import { ReceiveWebhookUseCase } from "@/app/projects/application/use-cases/webhooks/receive-webhook";
 import { ActivateUserUseCase } from "@/app/users/application/use-cases/users/activate-user";
 import { AuthenticateUserUseCase } from "@/app/users/application/use-cases/users/authenticate-user";
 import { CheckAuthenticatedUserUseCase } from "@/app/users/application/use-cases/users/check-authenticated-user";
 import { CreateUserUseCase } from "@/app/users/application/use-cases/users/create-user";
+import { GetUserByIdUseCase } from "@/app/users/application/use-cases/users/get-user-by-id";
 import { JsonWebToken } from "@/infra/adapters/jwt";
 import { IRepositoryFactory } from "@/infra/database";
 import { IMail } from "@/infra/providers/mail";
@@ -26,7 +29,22 @@ export class UseCaseFactory {
         return new CheckAuthenticatedUserUseCase({ repositoryFactory: this.repositoryFactory, jwt: this.jwt });
     }
 
+    getUserByIdUseCase() {
+        return new GetUserByIdUseCase({ repositoryFactory: this.repositoryFactory });
+    }
+
     createProjectUseCase() {
         return new CreateProjectUseCase({ repositoryFactory: this.repositoryFactory });
+    }
+
+    receiveWebhookUseCase() {
+        return new ReceiveWebhookUseCase({
+            repositoryFactory: this.repositoryFactory,
+            forwardWebhookUseCase: this.forwardWebhookUseCase(),
+        });
+    }
+
+    forwardWebhookUseCase() {
+        return new ForwardWebhookUseCase({ repositoryFactory: this.repositoryFactory, ws: null });
     }
 }
