1c04305457a84683a02d268aada10198896def46
João Vitor <joao_vitorsgs@hotmail.com>
2025-09-04 11:59:34 -0300

    feat(app/projects): add metodo 'destroyByProject' no contrato IWebhookLogRepository

--- src/app/projects/application/repos/webhook-log.repository.ts
+++ src/app/projects/application/repos/webhook-log.repository.ts
@@ -1,7 +1,10 @@
 import { IRepository } from "@/infra/database";
 
+import { ProjectDTO } from "../../domain/models/project";
 import { WebHookLogDTO } from "../../domain/models/webhook";
 
 export type WebhookLogWhereRepository = WebHookLogDTO;
 
-export type IWebhookLogRepository = IRepository<WebHookLogDTO, WebhookLogWhereRepository>;
+export interface IWebhookLogRepository extends IRepository<WebHookLogDTO, WebhookLogWhereRepository> {
+    destroyByProject(project: ProjectDTO): Promise<void>;
+}
