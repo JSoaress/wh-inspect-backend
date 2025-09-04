bedadf3f96520462c2ee3e354071069653df7e2e
João Vitor <joao_vitorsgs@hotmail.com>
2025-09-04 12:20:38 -0300

    feat(app/projects): add types ProjectMemberDTO e DetailedProjectDTO

--- src/app/projects/domain/models/project/project.dto.ts
+++ src/app/projects/domain/models/project/project.dto.ts
@@ -1,5 +1,6 @@
 import { AbstractModelProps } from "ts-arch-kit/dist/core/models";
 
+import { UserDTO } from "@/app/users/domain/models/user";
 import { z } from "@/infra/libs/zod";
 
 // TODO: alterar uuid do owner p/ v7
@@ -23,3 +24,9 @@ export type ProjectDTO = Required<AbstractModelProps> & z.output<Schema>;
 export type CreateProjectDTO = z.input<Schema>;
 
 export type UpdateProjectDTO = Partial<Omit<CreateProjectDTO, "slug">>;
+
+export type ProjectMemberDTO = Pick<UserDTO, "id" | "name">;
+
+export type DetailedProjectDTO = Omit<ProjectDTO, "members"> & {
+    members: ProjectMemberDTO[];
+};
