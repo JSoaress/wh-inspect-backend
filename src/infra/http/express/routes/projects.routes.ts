093601c6a4f33f1734c6b3374a49671ad32e9496
João Vitor <joao_vitorsgs@hotmail.com>
2025-09-04 12:22:04 -0300

    feat(infra/http): registra novas rotas em /projects

--- src/infra/http/express/routes/projects.routes.ts
+++ src/infra/http/express/routes/projects.routes.ts
@@ -5,11 +5,38 @@ import { ExpressRouter } from "../express-router";
 const projectsRoutes = new ExpressRouter("/projects", true);
 
 projectsRoutes.register({
+    method: "get",
+    path: "/",
+    useCase(factory) {
+        return factory.fetchProjectsUseCase();
+    },
+});
+projectsRoutes.register({
     method: "post",
     path: "/",
     statusCode: HttpStatusCodes.CREATED,
     useCase(factory) {
         return factory.createProjectUseCase();
+    },
+});
+projectsRoutes.register({
+    method: "patch",
+    path: "/:project",
+    buildInput(req) {
+        return { ...req.body, id: req.params.project };
+    },
+    useCase(factory) {
+        return factory.updateProjectUseCase();
+    },
+});
+projectsRoutes.register({
+    method: "delete",
+    path: "/:project",
+    buildInput(req) {
+        return { id: req.params.project };
+    },
+    useCase(factory) {
+        return factory.DeleteProjectUseCase();
     },
 });
 
