04922ae0cac08f5211531de56045ec2a8b5cf0a8
João Vitor <joao_vitorsgs@hotmail.com>
2025-09-05 13:32:46 -0300

    feat(infra/http): impl PlanJsonPresenter

--- src/infra/http/express/middlewares/format-response.middleware.ts
+++ src/infra/http/express/middlewares/format-response.middleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { HttpStatusCodes } from "ts-arch-kit/dist/http";
 
+import { Plan } from "@/app/subscription/domain/models/plan";
 import { User } from "@/app/users/domain/models/user";
 import * as presenters from "@/infra/presenters/json";
 
@@ -10,6 +11,8 @@ export function formatRespose(req: Request, res: Response) {
     switch (body.constructor) {
         case User:
             return res.status(statusCode || HttpStatusCodes.OK).json(presenters.userJsonPresenter.present(body));
+        case Plan:
+            return res.status(statusCode || HttpStatusCodes.OK).json(presenters.planJsonPresenter.present(body));
         default:
             return res.status(statusCode || HttpStatusCodes.OK).json(body);
     }
--- src/infra/presenters/json/index.ts
+++ src/infra/presenters/json/index.ts
@@ -1,5 +1,7 @@
+/* eslint-disable max-classes-per-file */
 import { IPresenter } from "ts-arch-kit/dist/core/helpers";
 
+import { Plan, PlanDTO } from "@/app/subscription/domain/models/plan";
 import { User, UserDTO } from "@/app/users/domain/models/user";
 
 type UserJson = Omit<UserDTO, "password" | "userToken" | "isAdmin">;
@@ -19,3 +21,27 @@ class UserJsonPresenter implements IPresenter<User, UserJson> {
 }
 
 export const userJsonPresenter = new UserJsonPresenter();
+
+type PlanJson = PlanDTO;
+
+class PlanJsonPresenter implements IPresenter<Plan, PlanJson> {
+    present(input: Plan): PlanJson {
+        return {
+            id: input.getId(),
+            name: input.name,
+            price: input.price,
+            isPaid: input.isPaid,
+            billingCycle: input.billingCycle,
+            eventsMonth: input.eventsMonth,
+            retention: input.retention,
+            replayEvents: input.replayEvents,
+            support: input.support,
+            createdAt: input.createdAt,
+            updatedAt: input.updatedAt,
+            visible: input.visible,
+            isActive: input.isActive,
+        };
+    }
+}
+
+export const planJsonPresenter = new PlanJsonPresenter();
