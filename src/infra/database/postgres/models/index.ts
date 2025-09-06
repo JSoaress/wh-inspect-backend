8db0e0585bfeaa4f64b2b1329fe4038200ff99f1
João Vitor <joao_vitorsgs@hotmail.com>
2025-09-06 16:46:23 -0300

    feat(infra/db): ajusta PlanPgMapper

--- src/infra/database/postgres/models/index.ts
+++ src/infra/database/postgres/models/index.ts
@@ -45,9 +45,10 @@ export type PgPlanDTO = PgModelId & {
     price: number;
     is_paid: boolean;
     billing_cycle: "monthly" | "quarterly" | "annual";
+    max_projects: number;
     events_month: number;
     retention: number;
-    replay_events: number;
+    replay_events: boolean;
     support: "community" | "email" | "priority";
     created_at: Date;
     updated_at: Date | null;
