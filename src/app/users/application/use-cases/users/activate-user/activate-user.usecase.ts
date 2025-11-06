8dd5c87c2235f284f7fdef182e0184f4355eeb68
João Vitor <joao_vitorsgs@hotmail.com>
2025-11-06 19:53:36 -0300

    feat(app/users): ativa assinatuta Freemium do usário após a ativação do mesmo

--- src/app/users/application/use-cases/users/activate-user/activate-user.usecase.ts
+++ src/app/users/application/use-cases/users/activate-user/activate-user.usecase.ts
@@ -1,7 +1,16 @@
-import { left, right } from "ts-arch-kit/dist/core/helpers";
+import { Either, left, right } from "ts-arch-kit/dist/core/helpers";
 import { UnitOfWork } from "ts-arch-kit/dist/database";
 
-import { MissingParamError, NotFoundModelError, UseCase } from "@/app/_common";
+import {
+    MissingParamError,
+    NotFoundModelError,
+    SystemParameterNotConfiguredError,
+    UndefinedFreemiumPlanError,
+    UseCase,
+    ValidationError,
+} from "@/app/_common";
+import { IParameterRepository } from "@/app/settings/application/repos";
+import { IPlanRepository, ISubscriptionRepository } from "@/app/subscription/application/repos";
 import { User } from "@/app/users/domain/models/user";
 
 import { IUserRepository } from "../../../repos";
@@ -11,15 +20,31 @@ import {
     ActivateUserUseCaseOutput,
 } from "./activate-user.usecase.types";
 
+type CreateSubscriptionToUserOutput = Either<
+    SystemParameterNotConfiguredError | UndefinedFreemiumPlanError | ValidationError,
+    void
+>;
+
 export class ActivateUserUseCase extends UseCase<ActivateUserUseCaseInput, ActivateUserUseCaseOutput> {
     private unitOfWork: UnitOfWork;
     private userRepository: IUserRepository;
+    private parameterRepository: IParameterRepository;
+    private planRepository: IPlanRepository;
+    private subscriptionRepository: ISubscriptionRepository;
 
     constructor({ repositoryFactory }: ActivateUserUseCaseGateway) {
         super();
         this.unitOfWork = repositoryFactory.createUnitOfWork();
         this.userRepository = repositoryFactory.createUserRepository();
-        this.unitOfWork.prepare(this.userRepository);
+        this.parameterRepository = repositoryFactory.createParameterRepository();
+        this.planRepository = repositoryFactory.createPlanRepository();
+        this.subscriptionRepository = repositoryFactory.createSubscriptionRepository();
+        this.unitOfWork.prepare(
+            this.userRepository,
+            this.parameterRepository,
+            this.planRepository,
+            this.subscriptionRepository
+        );
     }
 
     protected async impl({ token }: ActivateUserUseCaseInput): Promise<ActivateUserUseCaseOutput> {
@@ -29,8 +54,21 @@ export class ActivateUserUseCase extends UseCase<ActivateUserUseCaseInput, Activ
             if (!user) return left(new NotFoundModelError(User.name, { token }));
             const activateOrError = user.activate(token);
             if (activateOrError.isLeft()) return left(activateOrError.value);
-            await this.userRepository.save(user);
+            const activatedUser = await this.userRepository.save(user);
+            const subscriptionOrError = await this.createSubscriptionToUser(activatedUser);
+            if (subscriptionOrError.isLeft()) return left(subscriptionOrError.value);
             return right(undefined);
         });
+    }
+
+    private async createSubscriptionToUser(user: User): Promise<CreateSubscriptionToUserOutput> {
+        const parameter = await this.parameterRepository.findOne({ filter: { key: "subscription.plan.freemium" } });
+        if (!parameter) return left(new SystemParameterNotConfiguredError("subscription.plan.freemium"));
+        const plan = await this.planRepository.findById(parameter.value);
+        if (!plan) return left(new UndefinedFreemiumPlanError());
+        const subscriptionOrError = plan.subscribe(user, "free");
+        if (subscriptionOrError.isLeft()) return left(subscriptionOrError.value);
+        await this.subscriptionRepository.save(subscriptionOrError.value);
+        return right(undefined);
     }
 }
--- src/app/users/application/use-cases/users/activate-user/activate-user.usecase.types.ts
+++ src/app/users/application/use-cases/users/activate-user/activate-user.usecase.types.ts
@@ -1,6 +1,13 @@
 import { Either } from "ts-arch-kit/dist/core/helpers";
 
-import { InvalidTokenError, MissingParamError, NotFoundModelError } from "@/app/_common";
+import {
+    InvalidTokenError,
+    MissingParamError,
+    NotFoundModelError,
+    SystemParameterNotConfiguredError,
+    UndefinedFreemiumPlanError,
+    ValidationError,
+} from "@/app/_common";
 import { IRepositoryFactory } from "@/infra/database";
 
 export type ActivateUserUseCaseGateway = {
@@ -11,4 +18,12 @@ export type ActivateUserUseCaseInput = {
     token: string;
 };
 
-export type ActivateUserUseCaseOutput = Either<MissingParamError | NotFoundModelError | InvalidTokenError, void>;
+export type ActivateUserUseCaseOutput = Either<
+    | MissingParamError
+    | NotFoundModelError
+    | InvalidTokenError
+    | UndefinedFreemiumPlanError
+    | SystemParameterNotConfiguredError
+    | ValidationError,
+    void
+>;
