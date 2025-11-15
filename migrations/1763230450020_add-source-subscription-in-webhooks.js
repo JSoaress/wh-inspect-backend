f401ca31b59af9458cad6219c530344164fc393a
João Vitor <joao_vitorsgs@hotmail.com>
2025-11-15 15:45:56 -0300

    chore(infra/db): add migrations

---
+++ migrations/1763230450020_add-source-subscription-in-webhooks.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.addColumn("webhooks", {
+        source_subscription: { type: "uuid", notNull: true, references: '"subscriptions"' },
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropColumn("webhooks", "source_subscription");
+};
