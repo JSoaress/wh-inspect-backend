5d23e7d8356857590454ebdff63384b805ce6dfa
João Vitor <joao_vitorsgs@hotmail.com>
2025-11-15 15:40:57 -0300

    feat(app/projects): add metodo 'findSimplified' no contrato IWebhookLogRepository

--- src/app/projects/application/repos/webhook-log.repository.ts
+++ src/app/projects/application/repos/webhook-log.repository.ts
@@ -1,10 +1,13 @@
+import { QueryOptions } from "ts-arch-kit/dist/database";
+
 import { IRepository } from "@/infra/database";
 
 import { ProjectDTO } from "../../domain/models/project";
-import { WebHookLogDTO } from "../../domain/models/webhook";
+import { SimplifiedWebhook, WebHookLogDTO } from "../../domain/models/webhook";
 
 export type WebhookLogWhereRepository = WebHookLogDTO;
 
 export interface IWebhookLogRepository extends IRepository<WebHookLogDTO, WebhookLogWhereRepository> {
     destroyByProject(project: ProjectDTO): Promise<void>;
+    findSimplified(queryOptions?: QueryOptions<WebhookLogWhereRepository>): Promise<SimplifiedWebhook[]>;
 }
