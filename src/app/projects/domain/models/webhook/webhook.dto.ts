abd6d3bc3bb10a73278e2ca2058422816b996476
João Vitor <joao_vitorsgs@hotmail.com>
2025-11-15 15:40:07 -0300

    feat(app/projects): add attr 'sourceSubscription' em WebHookLogDTO e tbm type SimplifiedWebhook

--- src/app/projects/domain/models/webhook/webhook.dto.ts
+++ src/app/projects/domain/models/webhook/webhook.dto.ts
@@ -2,6 +2,8 @@ import { AbstractModelProps } from "ts-arch-kit/dist/core/models";
 
 import { z } from "@/infra/libs/zod";
 
+import { ProjectDTO } from "../project";
+
 export const WebHookLogSchema = z.object({
     projectId: z.uuid(),
     receivedFrom: z.coerce.string(),
@@ -14,6 +16,7 @@ export const WebHookLogSchema = z.object({
     replayedAt: z.coerce.date().nullish().default(null),
     replayStatus: z.enum(["success", "fail"]).nullish().default(null),
     targetUrl: z.coerce.string().nullish().default(null),
+    sourceSubscription: z.uuid(),
 });
 
 type Schema = typeof WebHookLogSchema;
@@ -21,3 +24,7 @@ type Schema = typeof WebHookLogSchema;
 export type WebHookLogDTO = Required<AbstractModelProps> & z.output<Schema>;
 
 export type CreateWebHookLogDTO = z.input<Schema>;
+
+export type SimplifiedWebhook = Pick<WebHookLogDTO, "id" | "receivedFrom" | "receivedAt"> & {
+    project: Pick<ProjectDTO, "id" | "name">;
+};
