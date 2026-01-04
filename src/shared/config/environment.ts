e598d93c325a1e0234317dc2479d96962b822cc7
João Vitor <joao_vitorsgs@hotmail.com>
2026-01-03 21:59:51 -0300

    feat(shared): add env var SELF_URL

--- src/shared/config/environment.ts
+++ src/shared/config/environment.ts
@@ -2,8 +2,9 @@ import envVar from "env-var";
 
 export const env = {
     NODE_ENV: envVar.get("NODE_ENV").default("development").asString(),
-    PLATFORM_NAME: envVar.get("PLATFORM_NAME").default("WebhookInspec").asString(),
+    PLATFORM_NAME: envVar.get("PLATFORM_NAME").default("HookHub").asString(),
     WEB_URL: envVar.get("WEB_URL").required().asUrlString(),
+    SELF_URL: envVar.get("SELF_URL").required().asUrlString(),
     MAIL_PROVIDER: envVar.get("MAIL_PROVIDER").required().asString(),
     MAIL_HOST: envVar.get("MAIL_HOST").required().asString(),
     MAIL_PORT: envVar.get("MAIL_PORT").required().asInt(),
