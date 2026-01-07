98efa8d745eb565fb44cea138da5f4b9927ad078
João Vitor <joao_vitorsgs@hotmail.com>
2026-01-07 12:13:14 -0300

    feat(app/users): implementa força de senha no vo Password

--- src/app/users/domain/models/user/password.vo.ts
+++ src/app/users/domain/models/user/password.vo.ts
@@ -1,13 +1,18 @@
 import bcrypt from "bcrypt";
-import { Either, right } from "ts-arch-kit/dist/core/helpers";
+import { Either, left, right } from "ts-arch-kit/dist/core/helpers";
 
 import { InvalidPasswordError } from "@/app/_common";
+import { env } from "@/shared/config/environment";
 
 export class Password {
     private constructor(private value: string) {}
 
     static async create(plainPassword: string): Promise<Either<InvalidPasswordError, Password>> {
-        // TODO: validar força da senha
+        if (!plainPassword) return left(new InvalidPasswordError("Senha não fornecida."));
+        if (env.NODE_ENV === "production" && /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])[^\s]{8,}$/.test(plainPassword)) {
+            const criteria = `A senha precisa conter mín. 8 caracteres, 1 maiúscula, 1 minúscula, 1 número, 1 caractere especial e sem espaços.`;
+            return left(new InvalidPasswordError(criteria));
+        }
         const hashPassword = await bcrypt.hash(plainPassword, 12);
         return right(new Password(hashPassword));
     }
