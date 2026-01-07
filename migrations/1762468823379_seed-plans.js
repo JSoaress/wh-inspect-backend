a019d74ce1b9c8c262a217461cbd1c1740b9c0c1
João Vitor <joao_vitorsgs@hotmail.com>
2026-01-07 12:19:40 -0300

    fix(infra/db): corrige dados de seeds na migration 'seed-plans'

--- migrations/1762468823379_seed-plans.js
+++ migrations/1762468823379_seed-plans.js
@@ -11,7 +11,7 @@ export const shorthands = undefined;
 export const up = (pgm) => {
     pgm.sql(`
     INSERT INTO plans (id,name,price,is_paid,billing_cycle,max_projects,events_month,retention,replay_events,"support",created_at,updated_at,visible,is_active) VALUES
-	 ('019a5698-2ced-7332-99ec-6b9ed99661de'::uuid,'Freenium',0.00,false,'monthly',1,50,3,false,'community','2025-11-05 21:35:33.449',NULL,true,true);
+	 ('019a5698-2ced-7332-99ec-6b9ed99661de'::uuid,'Freemium',0.00,false,'monthly',1,100,1,true,'community','2025-11-05 21:35:33.449',NULL,true,true);
   `);
 };
 
