3db0d22a9b41ebb2eef5f12f2de5616a3a149ab0
João Vitor <joao_vitorsgs@hotmail.com>
2026-01-10 18:23:34 -0300

    chore: remove lib dotenv e add @vitest/coverage-v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import dotenv from "dotenv";
 import path from "node:path";
 import { defineConfig } from "vitest/config";
-
-dotenv.config({ path: "./.env.test.local" });
 
 export default defineConfig({
     resolve: {
