b10a84bbc2e24581e536e395f7276fc958c42c57
João Vitor <joao_vitorsgs@hotmail.com>
2026-05-01 11:23:30 -0300

    feat(infra/db): add pg migration

---
+++ migrations/1774824915318_add-index-project-id-in-webhooks.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createIndex("webhooks", "project_id");
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropIndex("webhooks", "project_id");
+};
