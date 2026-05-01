eeecee740b8f21575a63695a27a723e3f894f634
João Vitor <joao_vitorsgs@hotmail.com>
2026-05-01 11:25:23 -0300

    test: add test unitario p/ entity User

--- src/app/users/domain/models/user/user.entity.spec.ts
+++ src/app/users/domain/models/user/user.entity.spec.ts
@@ -9,9 +9,20 @@ describe("user entity", () => {
             name: "Tony Stark",
             username: "tony",
             email: "tony@stark.com",
-            password: "i_love_papper",
+            password: "i_love_pepper",
         });
         expect(userOrError.isRight()).toBeTruthy();
         const user = userOrError.value as User;
+        expect(user.getId()).toBeDefined();
+        expect(user.name).toBe("Tony Stark");
+        expect(user.username).toBe("tony");
+        expect(user.email).toBe("tony@stark.com");
+        expect(user.password).toBeDefined();
+        expect(user.userToken?.startsWith("au")).toBeTruthy();
+        expect(user.cliToken?.startsWith("cli")).toBeTruthy();
+        expect(user.lastLogin).toBeNull();
+        expect(user.createdAt).toBeInstanceOf(Date);
+        expect(user.isAdmin).toBeFalsy();
+        expect(user.isActive).toBeFalsy();
     });
 });
